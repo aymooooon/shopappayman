1285729c11794068f9ea7df2c6cc27b2e93b7987
aymooooon <ahmed7744.ayman@gmail.com>
2021-10-17 20:00:38 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/shopappayman/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shopappayman
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:shopappayman/Form.dart';
+import 'package:shopappayman/Home.dart';
+import 'package:shopappayman/Homes.dart';
+void main(){
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Forma(),
+
+
+      )
+  );
+
+
+}
